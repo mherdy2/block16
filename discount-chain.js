9cf585ea6c2c9e2d1934c36dc2c30e8ed5090439
Maui Bautista <mherdy_2@hotmail.com>
2023-04-27 21:14:45 -0700

    block16

---
+++ discount-chain.js
@@ -0,0 +1,24 @@
+// If a customer has a subscription, the customer will receive a 25% discount after the refill total has been calculated.
+// If the customer has a coupon, the customer will receive a $10 discount after the subscription discount has been calculated.
+// At the end of the script, you should return and log the string "Your grand total is ${finalAmount}."
+const timmy = {
+  prescription: "acetaminophen",
+  pricePerRefill: 25,
+  refills: 3,
+  subscription: false,
+  coupon: true,
+};
+const sarah = {
+  prescription: "diphenhydramine",
+  pricePerRefill: 50,
+  refills: 1,
+  subscription: true,
+  coupon: false,
+};
+const rocky = {
+  prescription: "phenylephrine",
+  pricePerRefill: 30,
+  refills: 5,
+  subscription: true,
+  coupon: true,
+};
