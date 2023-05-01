dccfc600319cb94a1c3a804ce7188924ed8e6b4a
Maui Bautista <mherdy_2@hotmail.com>
2023-05-01 12:51:24 -0700

    functions

--- index.js
+++ index.js
@@ -20,10 +20,13 @@ const rocky = {
   prescription: "phenylephrine",
   pricePerRefill: 30,
   refills: 5,
-  subscription: true,
+  subscription: false,
   coupon: false,
 };
 
+// declaring variables for discounts
+let x = 0.25;
+let y = 10;
 // create a function that calculates the total price of prescription
 function totalPrice(pricePerRefill, refills) {
   return pricePerRefill * refills;
@@ -32,12 +35,12 @@ const totalAmount = totalPrice(rocky.pricePerRefill, rocky.refills);
 
 // create a function that adds the subscription/coupon discount to the total amount using if/else statements
 function addDiscounts(totalAmount) {
-  if (rocky.subscription === true && timmy.coupon === true) {
-    return totalAmount - totalAmount * 0.25 - 10;
+  if (rocky.subscription === true && rocky.coupon === true) {
+    return totalAmount - (totalAmount * [x] + y);
   } else if (rocky.subscription === true) {
-    return totalAmount - totalAmount * 0.25;
-  } else if (timmy.coupon === true) {
-    return totalAmount - 10;
+    return totalAmount - totalAmount * x;
+  } else if (rocky.coupon === true) {
+    return totalAmount - y;
   } else {
     return totalAmount;
   }
